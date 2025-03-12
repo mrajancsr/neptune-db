c5e099a62b6ce4d1584451e048644358d52acab1
mrajancsr <mrajancsr@gmail.com>
2025-03-12 11:45:40 -0400

    fixed impoorts

--- neptunedb/__init__.py
+++ neptunedb/__init__.py
@@ -1,4 +1,4 @@
-from neptunedb.dbhandler import AsyncDBReader, DBConfig, SyncDBReader  # noqa
+from neptunedb.dbhandler import AsyncDBHandler, DBConfig, SyncDBHandler  # noqa
 
 __version__ = "0.1.0"
 __author__ = "Rajan Subramanian"
